be8cbf6d00ed5aba11584fcc0765d1bd4202cb49
Bonnie121gb <bonifacecheruiyot121@gmail.com>
2025-10-31 10:00:57 +0300

    Add daily revenue calculation program
    This C program prompts the user to enter daily revenues for a week, calculates the total and average revenue, and displays the results.

--- 1d_array.c
+++ 1d_array.c
@@ -1,1 +1,29 @@
-// C programming
+//C program 
+/*
+Name: Cheruiyot Boniface
+Reg No: CT100/G/20094/23
+Description: prompt the user to enter daily revenues
+*/
+
+#include <stdio.h>
+int main(){
+float revenue[7];
+float total_revenue=0.0;
+float average_revenue;
+
+// revenue for each day 
+for(int i=0; i<7; i++){
+printf ("enter revenue for each day %d:", i+1);
+scanf("%f", & revenue[i]);
+total_revenue += revenue[i];
+}
+
+// average revenue 
+average_revenue = total_revenue / 7;
+
+//total and average revenue 
+printf("total revenue: %.2f \n", total_revenue);
+printf("average revenue: %.2f \n", average_revenue);
+
+return 0;
+}
