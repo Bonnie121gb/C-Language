5413eca06c3c2d5564637ee3ff32874af6d6df3b
Bonnie121gb <bonifacecheruiyot121@gmail.com>
2025-10-31 10:35:24 +0300

    Create loop.c

---
+++ loop.c
@@ -0,0 +1,20 @@
+//c program 
+/*
+Name: Cheruiyot Boniface 
+Reg No: CT100/G/20094/23
+Description: prompt the user to enter numbers from 100 to 50
+*/
+
+#include <stdio.h>
+
+int main(){
+
+   //for(start; stop; step){}
+   //for (initialization; condition; increment/decrement)
+   int i;
+   for(i=100; i>=50; i--) {
+   printf("%d \n", i);
+   }
+
+return 0 ;
+}
