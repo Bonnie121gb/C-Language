1c496f53e6fd1be6646857edfd5370538b0bc4cc
Bonnie121gb <bonifacecheruiyot121@gmail.com>
2025-10-31 11:02:07 +0300

    Create simple program to list IT units
    This C program lists semester one units for IT students.

---
+++ simple_program.c
@@ -0,0 +1,20 @@
+//C program
+/*
+Name: Cheruiyot Boniface 
+Reg: CT100/G/20094/23
+Description: Semester one units for IT
+*/
+#include<stdio.h>
+
+//main function 
+int main() {
+printf ("introduction to programming and alogarithms \n");
+printf ("analogue electronics \n");
+printf ("computer systems and organization \n");
+printf ("introduction to computer application n");
+printf ("essentials of accounting \n");
+printf ("mathematics for science \n");
+printf ("fundamentals of ICT \n");
+printf ("communication and information letracy skills \n");
+  return 0;
+}
